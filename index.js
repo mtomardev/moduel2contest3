189d3398b86569afb6b57b4196d69ce4ce74b571
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-01-21 20:12:33 +0530

    code

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ function validateForm(){
         returnval = false;
     }
 
-    
-    // let email = document.getElementById("text").value;
-    // let regx = /^([a-zA-Z0-9]+)@([a-zA-Z]).([a-z]{2,5})(.[a-z]{2,5})?$/;
-    // if (!regx.text(email)){
+    // let email = document.forms['myForm']["femail"].value;
+    // let regx = /^([a-zA-Z0-9]+)@([a-zA-Z]).([a-z]{2,5})(.[a-z]{2,5})?$/
+    // if (!regx.text(email)==-1){
     //     seterror("email", "*Please provide valid email addres");
     //     returnval = false;
-    // }
+    // }    
+
 
     let password = document.forms['myForm']["fpass"].value;
     if (password.length < 6){
@@ -39,6 +39,32 @@ function validateForm(){
         seterror("pass", "*Password should be atleast 6 characters long!");
         returnval = false;
     }
+    if(password.search(/[a-z]/)==-1){
+        seterror("pass", "*Password should have atleast 1 Lower case character!");
+        returnval = false;
+    }
+
+    if(password.search(/[A-Z]/)==-1){
+        seterror("pass", "*Password should have atleast 1 Upper case character!");
+        returnval = false;
+    }
+
+    if(password.search(/[!\@\#\%]/)==-1){
+        seterror("pass", "*Password should have atleast 1 special character eample ! @ # $ %   !");
+        returnval = false;
+    }
+
+    if(password.search(/[0-9]/)==-1){
+        seterror("pass", "*Password should have atleast 1 numeric character!");
+        returnval = false;
+    }
+
+    
+
+
+
+
+
 
     let cpassword = document.forms['myForm']["fcpass"].value;
     if (cpassword != password){
