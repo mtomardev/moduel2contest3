dafd8d5b743c06d54e97dab04b9ab2003ddb5c35
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-01-21 17:51:51 +0530

    second

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+}
+
+
 function seterror(id, error){
     //sets error inside tag of id 
     element = document.getElementById(id);
@@ -8,12 +18,33 @@ function seterror(id, error){
 
 function validateForm(){
     let returnval = true;
+    clearErrors();
     let name = document.forms['myForm']["fname"].value;
     if (name.length<2){
         seterror("name", "*Length of name is too short");
         returnval = false;
     }
 
+    
+    // let email = document.getElementById("text").value;
+    // let regx = /^([a-zA-Z0-9]+)@([a-zA-Z]).([a-z]{2,5})(.[a-z]{2,5})?$/;
+    // if (!regx.text(email)){
+    //     seterror("email", "*Please provide valid email addres");
+    //     returnval = false;
+    // }
+
+    let password = document.forms['myForm']["fpass"].value;
+    if (password.length < 6){
+
+        seterror("pass", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+
+    let cpassword = document.forms['myForm']["fcpass"].value;
+    if (cpassword != password){
+        seterror("cpass", "*Password and Confirm password should match!");
+        returnval = false;
+    }
 
 
 
